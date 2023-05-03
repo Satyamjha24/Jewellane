c949225d8b1e17f90653263347ef640a4f08ebcb
Satyamjha24 <pananrocks18@gmail.com>
2023-05-03 19:14:22 +0530

    base_structure

---
+++ frontend/src/routes/AllRoutes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const AllRoutes = () => {
+  return (
+    <div>
+        <Routes>
+            <Route></Route>
+        </Routes>
+    </div>
+  )
+}
+
+export default AllRoutes

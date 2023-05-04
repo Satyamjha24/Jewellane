00ab97819299bc3498bc59f40665082f74aa9b3f
Ajay84sia <ajay9679339@gmail.com>
2023-05-04 14:12:30 +0530

    admin register & login done

---
+++ backend/middleware/adminauth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const adminauth = (req, res, next) => {
+    const token = req.headers.authorization
+    if (token) {
+        try {
+            const decoded = jwt.verify(token.split(" ")[1], "admin");
+            if (decoded) {
+                req.body.adminEmail = decoded.adminEmail
+                req.body.adminType = decoded.adminType
+                next()
+            } else {
+                res.send({ "msg": "Please Login!!" })
+            }
+
+        } catch (error) {
+            res.send({ "err": error.message })
+        }
+
+    }
+}
+
+module.exports = {
+    adminauth
+}
---
+++ backend/routes/admin.route.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { AdminModel } = require("../model/admin.model")
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const adminRouter = express.Router()
+
+
+
+adminRouter.post("/register", async (req, res) => {
+    //Logic
+    const { name, email, password, type } = req.body
+
+    try {
+        bcrypt.hash(password, 5, async (err, hash) => {
+            // Store hash in your password DB.
+            const user = new AdminModel({ name, email, type, password: hash })
+            await user.save()
+            res.status(200).send({ "msg": "New user has been registered" })
+        });
+
+    } catch (err) {
+        res.status(400).send({ "err": err.message })
+    }
+
+})
+
+
+
+adminRouter.post("/login", async (req, res) => {
+    //Logic
+    const { email, password } = req.body
+    try {
+        const user = await AdminModel   .findOne({ email })
+        if (user) {
+            bcrypt.compare(password, user.password, (err, result) => {
+                // result == true
+                if (result) {
+                    const token = jwt.sign({ adminEmail: user.email, adminType: user.type }, "admin");
+                    res.status(200).send({ "msg": "Login Succesfull", token })
+                } else {
+                    res.status(200).send({ "msg": "Wrong Credentials!!!" })
+                }
+            });
+
+        } else {
+            res.status(200).send({ "msg": "Wrong Credentials!!!" })
+        }
+    } catch (error) {
+        res.status(400).send({ "err": err.message })
+    }
+})
+
+
+
+module.exports = {
+    adminRouter
+}

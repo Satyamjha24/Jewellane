e063a7dd5556c02c839430effcb4f5aa7920e370
harshita <harshitakatara34>
2023-05-05 16:10:19 +0530

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './Pages/Product';
 
 function App() {
   return (
     <div className="App">
+    <Product/>
     </div>
   );
 }
--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Bracelet from '../Pages/Bracelet'
+import Mangalsutra from '../Pages/Mangalsutra'
+import Rings from '../Pages/Rings'
 
 const AllRoutes = () => {
   return (
     <div>
         <Routes>
-            <Route></Route>
+            <Route path="/bracelet"><Bracelet/></Route>
+            <Route path="/Rings"><Rings/></Route>
+            <Route path="/Mangalsutra"><Mangalsutra/></Route>
         </Routes>
     </div>
   )

2d0996a1d6047d6efd77dae074f6c73f4108824c
harshita <harshitakatara34>
2023-05-06 23:24:42 +0530

    d

--- frontend/src/redux/Product/action.jsx
+++ frontend/src/redux/Product/action.jsx
@@ -1,10 +1,11 @@
 import axios from"axios"
 import { PRODUCT_FAILURE, PRODUCT_REQUEST, PRODUCT_SUCCESS } from "./actionType"
 
-export const getProduct=(dispatch)=>{
+export const getProduct=(obj)=>(dispatch)=>{
+    console.log(obj)
     dispatch({type:PRODUCT_REQUEST})
-    axios.get("https://taupe-waistcoat.cyclic.app/ring/").then((res)=>{
-        console.log(res)
+    axios.get(`https://taupe-waistcoat.cyclic.app/ring`,obj).then((res)=>{
+        // console.log(res)
         dispatch({type:PRODUCT_SUCCESS,payload:res.data})
     })
     .catch(()=>{

0d42ebef0b73b4e0cdaf46d0efadbd6f505b7fa4
harshita <harshitakatara34>
2023-05-07 15:09:55 +0530

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 import Product from './Pages/Product';
 import CartPage from './Pages/CartPage/CartPage';
+import SingleuserPage from './Pages/SingleuserPage';
+import AllRoutes from './routes/AllRoutes';
 
 function App() {
   return (
     <div className="App">
+    <AllRoutes/>
     <Product/>
       <CartPage/>
-    </div>
+</div>
   );
 }
 
--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -3,14 +3,14 @@ import { Route, Routes } from 'react-router-dom'
 import Bracelet from '../Pages/Bracelet'
 import Mangalsutra from '../Pages/Mangalsutra'
 import Rings from '../Pages/Rings'
+import SingleuserPage from '../Pages/SingleuserPage'
 
 const AllRoutes = () => {
   return (
     <div>
         <Routes>
-            <Route path="/bracelet"><Bracelet/></Route>
-            <Route path="/Rings"><Rings/></Route>
-            <Route path="/Mangalsutra"><Mangalsutra/></Route>
+          
+            <Route path="/ring/:ring_id" element={<SingleuserPage/>}></Route>
         </Routes>
     </div>
   )
